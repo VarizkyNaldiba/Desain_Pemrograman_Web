ff1117f76400db22096795022b0e33a5b96e262f
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2024-09-19 09:15:15 +0700

    php

---
+++ dasarWeb/PHP/operator.php
@@ -0,0 +1,45 @@
+<?php 
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$pangkat= $a ** $b;
+
+echo "Hasil Tambah : $hasilTambah <br>";
+echo "Hasil Kurang : $hasilKurang <br>";
+echo "Hasil Kali : $hasilKali <br>";
+echo "Hasil Bagi : $hasilBagi <br>";
+echo "Hasil Pangkat : $pangkat <br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+
+echo "<br>";
+echo "Hasil Perbandingan $a dan $b Sama :" . ($hasilSama ? 'True' : 'False') . "<br>";
+echo "Hasil Perbandingan $a dan $b Tidak Sama :" .($hasilTidakSama ? 'True' : 'False') . "<br>";
+echo "Hasil Perbandingan $a dan $b Lebih Kecil :" . ($hasilLebihKecil ? 'True' : 'False') . "<br>";
+echo "Hasil Perbandingan $a dan $b Lebih Besar : " .($hasilLebihBesar ? 'True' : 'False') . "<br>";
+echo "Hasil Perbandingan $a dan $b Lebih Kecil Sama :" . ($hasilLebihKecilSama ? 'True' : 'False') . "<br>";
+echo "Hasil Perbandingan $a dan $b Lebih Besar Sama :" .($hasilLebihBesarSama ? 'True' : 'False') . "<br>";
+
+$hasilAnd = $a && $b;
+$hasilOr = $a || $b;
+$hasilNotA = !$a;
+$hasilNotB = !$b;
+
+echo "<br>";   
+echo "Hasil Operasi AND : " . ($hasilAnd ? 'True' : 'False') . "<br>";
+echo "Hasil Operasi OR : " . ($hasilOr ? 'True' : 'False') . "<br>";
+echo "Hasil Operasi NOT A : " . ($hasilNotA ? 'True' : 'False') . "<br>";
+echo "Hasil Operasi NOT B : " . ($hasilNotB ? 'True' : 'False') . "<br>";
+
+?>
+
---
+++ dasarWeb/PHP/variabel_konstanta.php
@@ -0,0 +1,15 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+echo "Hasil Penjumlahan  $angka1 dan $angka2 adalah $hasil";
+
+$benar = true;
+$salah = false;
+echo "Variabel benar : $benar , Variabel salah : $salah";
+
+define("Nama_Situs"," Websiteku.com " );
+define("Tahun_Pendirian"," 2023 ");
+
+echo "Selamat datang di" . Nama_Situs .  ", Situs yang didirikan pada tahun" . Tahun_Pendirian . ".";
+?> 
