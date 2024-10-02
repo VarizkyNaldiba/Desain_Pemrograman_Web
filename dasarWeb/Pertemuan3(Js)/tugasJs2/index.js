a3350efa039156e12ae713a52ceea89be61338a5
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2024-10-02 20:17:32 +0700

    disuruh sarah tapi fail

--- dasarWeb/Pertemuan3(Js)/tugasJs2/index.js
+++ dasarWeb/Pertemuan3(Js)/tugasJs2/index.js
@@ -1,15 +1,20 @@
 document.getElementById("Siform").addEventListener("submit", function (e) {
     e.preventDefault();
 
+    var valid = true;
+
     var nama = document.getElementById("inputNama").value;
     var alamat = document.getElementById("inputAlamat").value;
     var tanggalLahir = document.getElementById("inputTanggalLahir").value;
-    // var genderlakilaki = document.getElementById("genderlakilaki");
-    // var gendercewe = document.getElementById("gendercewe");
-    var jenisKelamin = document.getElementById("genderlakilaki").checked || document.getElementById("gendercewe").checked;
+    var jenisKelamin = document.querySelector('input[name="gender"]:checked');
     var hobby = document.getElementById("inputHobby").value;
     var pekerjaan = document.getElementById("inputPekerjaan").value;
-    let valid = true;
+    var checkMeOut = document.getElementById("gridCheck").checked;
+
+    if (!checkMeOut) {
+        alert("Harus Check Me Out");
+        valid = false;
+    }
 
     if (nama === "") {
         alert("Nama tidak boleh kosong yaa");
@@ -25,23 +30,17 @@ document.getElementById("Siform").addEventListener("submit", function (e) {
         alert("Tanggal Lahir tidak boleh kosong yaa");
         valid = false;
     }
-// console.log(jenisKelamin);
+
     if (!jenisKelamin) {
         alert("Jenis Kelamin tidak boleh kosong yaa");
         valid = false;
     }
-    // if (hobby === "") {
-    //     alert("Hobby tidak boleh kosong yaa");
-    //     valid = false;
-    // }
-    // if (pekerjaan === "") {
-    //     alert("Pekerjaan tidak boleh kosong yaa");
-    //     valid = false; 
-    // }
 
     if (valid) {
         alert("Selamat Datang " + nama);
+    } else {
+        alert("Mohon isi data dengan benar");
     }
-   
 });
+
 
