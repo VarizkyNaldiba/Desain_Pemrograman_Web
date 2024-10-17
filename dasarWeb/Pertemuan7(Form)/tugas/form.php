156bea5f2d622906c297139629e9d5b5a4c819b3
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2024-10-17 07:41:59 +0700

    ye

--- dasarWeb/Pertemuan7(Form)/tugas/form.php
+++ dasarWeb/Pertemuan7(Form)/tugas/form.php
@@ -1,16 +1,21 @@
-<link rel="stylesheet" href="formphp.css">
+
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Ambil semua input nama yang dikirim
-    $indeks = count($_POST); // Jumlah nama user yang diisi
+    $indeks = count($_POST) / 3; 
     
     echo "<h2>Data yang Dikirim</h2>";
+    echo "<table border='1' cellpadding='5' cellspacing='0'>";
+    echo "<tr><th>Nama</th><th>Alamat</th><th>Asal Daerah</th></tr>";
     for ($i = 1; $i <= $indeks; $i++) {
-        if (isset($_POST["nama_$i"])) {
-            $nama = $_POST["nama_$i"];
-            echo "<p>Nama User $i: " . htmlspecialchars($nama) . "</p>";
+        if (isset($_POST["nama_$i"]) && isset($_POST["alamat_$i"]) && isset($_POST["asal_$i"])) {
+            $nama = ($_POST["nama_$i"]);
+            $alamat = ($_POST["alamat_$i"]);
+            $asal = ($_POST["asal_$i"]);
+            echo "<tr><td>$nama</td><td>$alamat</td><td>$asal</td></tr>";
         }
     }
+    echo "</table>";
 }
 ?>
 
