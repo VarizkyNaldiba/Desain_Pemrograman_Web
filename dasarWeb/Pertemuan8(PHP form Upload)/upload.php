e41b2b0eeaa7206db222a1f30c9d7187a488239e
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2024-10-17 10:06:37 +0700

    y

--- dasarWeb/Pertemuan8(PHP form Upload)/upload.php
+++ dasarWeb/Pertemuan8(PHP form Upload)/upload.php
@@ -4,17 +4,18 @@ if (isset($_POST["submit"])) {
     $targetfile = $target_dir . basename($_FILES["myfile"]["name"]);
     $filetype = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-    $maxsize = 5*1024*1024;
-    if  (in_array($filetype, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $maxsize = 3*1024*1024;
+    if (in_array($filetype, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
             echo "File Uploaded";
+
+            echo '<img src="' . $targetfile . '" style="width: 200px; height: auto;" alt="Thumbnail">';
         } else {
             echo "File Not Uploaded";
         }
     } else {
-        echo "File Not Allowed";      
+        echo "File Not Allowed";
+    }
 }
-}
-
 ?>
