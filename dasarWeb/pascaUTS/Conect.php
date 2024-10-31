a1bb87fe8bffb28294ea098de338bf67a37e1eef
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2024-10-31 10:54:44 +0700

    1/8

---
+++ dasarWeb/pascaUTS/Conect.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "DESKTOP-ONR2FF9\VARIZKY03";
+$array = array(
+  "database" => "DB_Provinsi",
+
+);
+// Create connection
+$conn = sqlsrv_connect($servername, $array);
+
+// Check connection
+if ($conn) {
+  echo "Connected successfully\n";
+} else {
+  echo "Connection could not be established.\n";
+}
+
+?>
---
+++ dasarWeb/pascaUTS/index.php
@@ -0,0 +1,43 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Provinsi Provinsi Indonesia</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand text-center " id="logo" href="tatib.php" ><b>Provinsi Indonesia</b></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="tatib.php">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        
+        <!-- Tabel tata tertib -->
+        <table class="table table-striped text-center">
+            <thead>
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Nama Provinsi</th>
+                    <th scope="col">Kode Pos </th>
+                    <th scope="col">Ibu Kota </th>
+                </tr>
+            </thead>
+</body>
+</html>
