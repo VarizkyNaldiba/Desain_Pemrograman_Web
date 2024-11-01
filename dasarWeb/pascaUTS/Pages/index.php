7257939456cb56d7058fc64dd9842cbd3cc0dd38
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2024-11-01 21:24:34 +0700

    dahla

--- dasarWeb/pascaUTS/Pages/index.php
+++ dasarWeb/pascaUTS/Pages/index.php
@@ -1,72 +1,58 @@
 <?php
-include "ConnectToDB.php";
+include 'ConnectToDB.php';
+
 $query = "SELECT * FROM Provinsi";
 $result = sqlsrv_query($conn, $query);
 
-?>
+if (!$result) {
+    die("Query gagal: " . print_r(sqlsrv_errors(), true));
+}
 
+$i = 1;
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Provinsi Provinsi Indonesia</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-        <link rel="stylesheet" href="./style/style.css">
+    <title>Provinsi Indonesia</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand text-center " id="logo" href="tatib.php"><b>Provinsi Indonesia</b></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="tatib.php">Home</a>
-                    </li>
-                </ul>
-            </div>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><b>Provinsi Indonesia</b></a>
         </div>
     </nav>
 
-    <div class="container">
-
-        <!-- Tabel tata tertib -->
-        <table class="table table-striped text-center">
+    <div class="container mt-4">
+        <a href="Create.php" class="btn btn-success mb-3">Tambah Provinsi</a>
+        <table class="table table-striped table-hover">
             <thead>
                 <tr>
                     <th scope="col">No</th>
                     <th scope="col">Nama Provinsi</th>
-                    <th scope="col">Ibu Kota Provinsi </th>
+                    <th scope="col">Ibu Kota Provinsi</th>
                     <th scope="col">Aksi</th>
                 </tr>
             </thead>
-
             <tbody>
-
-                <?php
-                $i = 1;
-                while ($data = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))  {
-
-                    ?>
+                <?php while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)): ?>
                     <tr>
-                        <th scope="row"><?= $i++ ?></th>
-                        <td><?= $data['nama_provinsi'] ?></td>
-                        <td><?= $data['ibukota'] ?></td>
-                        <td  type="button" class="btn btn-info"> Edit </button></td>
-                        <td  type="button" class="btn btn-danger"> Hapus </button></td>
+                        <th scope="row"><?= $i ?></th>
+                        <td><?= htmlspecialchars($row['nama_provinsi']) ?></td>
+                        <td><?= htmlspecialchars($row['ibukota']) ?></td>
+                        <td>
+                            <a href="Edit.php?id=<?= $row[$i] ?>" class="btn btn-primary">Edit</a>
+                            <a href="Delete.php?id=<?= $row[$i] ?>" class="btn btn-danger" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?');">Hapus</a>
+                        </td>
                     </tr>
-                    
-                    <?php
-                }
-                ?>
+                    <?php $i++; endwhile; ?>
             </tbody>
-        </body>
-        
+        </table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</body>
 </html>
