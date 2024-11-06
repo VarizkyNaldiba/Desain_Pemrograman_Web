2d2f20730bf5bdd71aaa3c0fffe3d6a40904b9c9
VarizkyNaldiba <138732792+VarizkyNaldiba@users.noreply.github.com>
2024-11-06 21:55:42 +0700

    lupa css'

--- dasarWeb/pascaUTS/Pages/index.php
+++ dasarWeb/pascaUTS/Pages/index.php
@@ -98,6 +98,7 @@ if (isset($_GET['aksi']) && $_GET['aksi'] == 'delete') {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <link rel="stylesheet" href="style.css">
     <script>
         $(document).ready(function () {
             $("#flip").click(function () {
